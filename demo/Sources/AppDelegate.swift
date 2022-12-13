5ead88731e3c708952c9ca6d9d38caadc36e2724
Ivan Rysev <ivan.rysev@gmail.com>
2022-12-13 13:45:46 +0300

    v2 first

--- demo/Sources/AppDelegate.swift
+++ demo/Sources/AppDelegate.swift
@@ -6,7 +6,8 @@
 //
 
 import UIKit
-import Cloudtips
+import CloudtipsSDK
+//import YandexPaySDK
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         do {
             let yaAppId = "1193a702-d3c0-4637-a7c0-2ac95b73ee29"
-            try CloudtipsSDK.initialize(yandexPayAppId: yaAppId, sandboxMode: true)
+            try Cloudtips.initialize(yandexPayAppId: yaAppId, sandboxMode: true)
+
+                // Укажите конфигурацию
+//            let merchant = YandexPaySDKMerchant(
+//                // ID продавца в системе YandexPay
+//                id: "1193a702-d3c0-4637-a7c0-2ac95b73ee29",
+//                // Имя продавца
+//                name: "cloudpayments",
+//                // URL продавца
+//                url: "https://cloudtips.ru"
+//            )
+//
+//            let configuration = YandexPaySDKConfiguration(
+//                // Необходимое окружение
+//                environment: .sandbox,
+//                // Информация о мерчанте
+//                merchant: merchant,
+//                // Локализация
+//                locale: .ru
+//            )
+//                // Инициализируйте SDK
+//            try YandexPaySDKApi.initialize(configuration: configuration)
+
         } catch {
             fatalError("Unable to initialize CloudtipsSDK.")
         }
@@ -25,21 +48,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        CloudtipsSDK.instance.applicationDidReceiveUserActivity(userActivity)
+        Cloudtips.instance.applicationDidReceiveUserActivity(userActivity)
         return true
     }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        CloudtipsSDK.instance.applicationDidReceiveOpen(url, sourceApplication: options[.sourceApplication] as? String)
+        Cloudtips.instance.applicationDidReceiveOpen(url, sourceApplication: options[.sourceApplication] as? String)
         return true
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        CloudtipsSDK.instance.applicationWillEnterForeground()
+        Cloudtips.instance.applicationWillEnterForeground()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        CloudtipsSDK.instance.applicationDidBecomeActive()
+        Cloudtips.instance.applicationDidBecomeActive()
     }
     
 }
--- demo/Sources/ViewController.swift
+++ demo/Sources/ViewController.swift
@@ -6,27 +6,48 @@
 //
 
 import UIKit
-import Cloudtips
+import CloudtipsSDK
+//import sdk
+
+extension Bundle {
+    class var tipsSdk: Bundle {
+        let bundle = Bundle.init(for: ViewController.self)
+        let bundleUrl = bundle.url(forResource: "CloudtipsSDKResources", withExtension: "bundle")
+        let resBundle = Bundle.init(url: bundleUrl!)
+        return resBundle!
+    }
+}
+
+
 
 class ViewController: UIViewController {
     @IBOutlet private weak var textField: TextField!
     @IBOutlet private weak var continueButton: Button!
 
+    //lazy var cloud = Cloud()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.textField.didChange = {
-            if let text = self.textField.text {
-                self.textField.text = text.formattedPhoneNumber()
-            }
-            
-            self.validate()
-        }
-        self.validate()
-        
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
+
+//        self.textField.didChange = {
+//            if let text = self.textField.text {
+//                self.textField.text = text.formattedPhoneNumber()
+//            }
+//
+//            self.validate()
+//        }
+//        self.validate()
+//
+//        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+//        tap.cancelsTouchesInView = false
+//        view.addGestureRecognizer(tap)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //cloud.show(from: self)
     }
     
     @objc func hideKeyboard() {
@@ -34,40 +55,42 @@ class ViewController: UIViewController {
     }
     
     private func validate(){
-        if let text = self.textField.text?.phoneNumber(), text.count == 11 {
-            self.continueButton.isEnabled = true
-        } else {
-            self.continueButton.isEnabled = false
-        }
+//        if let text = self.textField.text?.phoneNumber(), text.count == 11 {
+//            self.continueButton.isEnabled = true
+//        } else {
+//            self.continueButton.isEnabled = false
+//        }
     }
 
     @IBAction func onContinue(_ sender: UIButton) {
-        if let text = self.textField.text?.phoneNumber() {
+        if let text = self.textField.text {
 
+            let user =  CloudtipsUser(layoutId: text, name: "Cloudtips demo user", agentCode: "ctdemo")
 
-//            let configuration = TipsConfiguration(phoneNumber: "+" + text, userName: "Cloudtips demo user", partner: "ctdemo")
+            let configuration = CloudtipsConfiguration(user: user,
+                                                       applePayMerchantId: "merchant.ru.cloudpayments",
+                                                       style: nil,
+                                                       delegate: self,
+                                                       testMode: false)
 
-            let configuration = TipsConfiguration(phoneNumber: "+" + text,
-                                                  userName: "Cloudtips demo user",
-                                                  partner: "ctdemo",
-                                                  testMode: true)
+            //            configuration.setApplePayMerchantId("merchant.ru.cloudpayments")
+            //            configuration.setTipsDelegate(self)
 
-            configuration.setApplePayMerchantId("merchant.ru.cloudpayments")
-            configuration.setTipsDelegate(self)
+            //            configuration.navigationTintColor = .red
+            //            configuration.navigationBackgroundColor = .yellow
 
-//            configuration.navigationTintColor = .red
-//            configuration.navigationBackgroundColor = .yellow
+            Cloudtips.instance.show(with: configuration, from: self)
 
-            TipsViewController.present(with: configuration, from: self)
         }
     }
+
 }
 
 extension ViewController: TipsDelegate {
     func onTipsSuccessed() {
         print("Чаевые получены")
     }
-    
+
     func onTipsCancelled() {
         print("Пользователь закрыл форму")
     }
